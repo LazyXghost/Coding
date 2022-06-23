9f09e8d6e10c0fa1a83c2e3c73a7a3d619a61640
lazyXghost <rjtbnslrealme@gmail.com>
2022-06-23 19:24:52 +0530

    Removed bug in dsu

--- Contest/code.cpp
+++ Contest/code.cpp
@@ -39,78 +39,70 @@ template <class T> void _print(multiset <T> v) {cerr << "{ "; for (T i : v) {_pr
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {cerr<< "(";_print(i.first); cerr << "--> "; _print(i.second);cerr<< "),";} cerr << "]";}
 template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {cerr<< "(";_print(i.first); cerr << "--> "; _print(i.second);cerr<< "),";} cerr << "]";}
 
+// ============================= DSU ================================
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+class dsu
+{
+    map<int,int> parent, size;
+
+public:
+    // making new set by just initialising the parent of set as the same number
+    void make_set(int v)
+    {
+        parent[v] = v;
+        size[v] = 1;
+    }
+
+    // finding the root of the set
+    int find_set(int v)
+    {
+        if (v == parent[v])
+            return v;
+        // return find_set(parent[v]);
+
+        return parent[v] = find_set(parent[v]);
+        // 1st optimisization - path compression
+        // directly connects all the linear chain nodes to parent
+        // 7->5->3->1 ==> 7->1,5->1,3->1
+    }
+
+    // making the root of one set as the parent of root of other set will combine two sets
+    void union_set(int a, int b)
+    {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b)
+        {
+            // 2nd optimisation - union by size
+            if (size[a] < size[b])
+                swap(a, b);
+
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+
+    // returning the size of the set of vertex v
+    int find_size(int v)
+    {
+        return size[find_set(v)];
+    }
 };
+/*
+Time complexity -
+                            AFTER OPTIMISATION'S
+    FIND SET  - O(n)      O(alpha(n)) - almost constant
+    MAKE SET  - O(1)              O(1)
+    UNION SET - O(n)           O(alpha(n))
+*/
+//==================================================================
+
+
 struct solution{
-
-    void quickSortList(ListNode *head, int len){
-        if(head == NULL || len == 0)
-            return;
-
-        int pivot = head->val;
-        printList(head);
-        cout<<"PIVOT IS ==>"<<pivot<<"\n";
-        ListNode *curr = head->next, *left = head;
-        int i = 1;
-        int leftCount = 0;
-        while(i++ < len){
-            if(curr->val <= pivot){
-                leftCount++;
-                left = left->next;
-                swap(left->val, curr->val);
-            }
-            curr = curr->next;
-        }
-        swap(head->val, left->val);
-        quickSortList(head, leftCount);
-        if(left->next != NULL)
-            quickSortList(left->next, len - leftCount - 1);
-    }
-
-    ListNode* sortList(ListNode* A) {
-        cout<<"LINKED LIST==>";
-        int len = 0;
-        ListNode *curr = A;
-        while(curr != NULL){
-            len++;
-            curr = curr->next;
-        }
-        quickSortList(A, len);
-        return A;
-    }
     ll n;
-    void printList(ListNode *head){
-        ListNode *curr = head;
-        while(curr != NULL){
-            cout<<curr->val;
-            space;
-            curr = curr->next;
-        }
-        newline;
-    }
     solution(){
         cin>>n;
-        ListNode *head = NULL;
-        ListNode *end = head;
-        while(n--){
-            int x;
-            cin>>x;
-            ListNode *tmp = new ListNode(x);
-            if(head == NULL){
-                head = tmp;
-                end = head;
-            }
-            else{
-                end -> next = tmp;
-                end = tmp;
-            }
-        }
-        sortList(head);
-        printList(head);
+        
     }
 };
 int main()
