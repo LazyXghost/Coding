e84f7e077681901d591f92203e2d1bdb9c574cba
lazyXghost <rjtbnslrealme@gmail.com>
2022-07-11 15:18:01 +0530

    Added changes

--- Contest/code.cpp
+++ Contest/code.cpp
@@ -4,17 +4,9 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
-#define p(a,b) pair<a,b>
-#define pll pair<ll,ll>
-#define v(a) vector<a>
-#define vll vector<ll>
-#define s(a) set<a>
-#define ms(a) multiset<a>
-#define m(a,b) map<a,b>
-#define mm(a,b) multimap<a,b>
-
 #define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define tests(t) ll t; cin >> t; while(t--)
+#define val(x) (*(x))
 #define space cout<<" ";
 #define newline cout<< "\n";
 
@@ -39,67 +31,8 @@ template <class T> void _print(multiset <T> v) {cerr << "{ "; for (T i : v) {_pr
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {cerr<< "(";_print(i.first); cerr << "--> "; _print(i.second);cerr<< "),";} cerr << "]";}
 template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {cerr<< "(";_print(i.first); cerr << "--> "; _print(i.second);cerr<< "),";} cerr << "]";}
 
-// ============================= DSU ================================
-
-class dsu
-{
-    map<int,int> parent, size;
-
-public:
-    // making new set by just initialising the parent of set as the same number
-    void make_set(int v)
-    {
-        parent[v] = v;
-        size[v] = 1;
-    }
-
-    // finding the root of the set
-    int find_set(int v)
-    {
-        if (v == parent[v])
-            return v;
-        // return find_set(parent[v]);
-
-        return parent[v] = find_set(parent[v]);
-        // 1st optimisization - path compression
-        // directly connects all the linear chain nodes to parent
-        // 7->5->3->1 ==> 7->1,5->1,3->1
-    }
-
-    // making the root of one set as the parent of root of other set will combine two sets
-    void union_set(int a, int b)
-    {
-        a = find_set(a);
-        b = find_set(b);
-        if (a != b)
-        {
-            // 2nd optimisation - union by size
-            if (size[a] < size[b])
-                swap(a, b);
-
-            parent[b] = a;
-            size[a] += size[b];
-        }
-    }
-
-    // returning the size of the set of vertex v
-    int find_size(int v)
-    {
-        return size[find_set(v)];
-    }
-};
-/*
-Time complexity -
-                            AFTER OPTIMISATION'S
-    FIND SET  - O(n)      O(alpha(n)) - almost constant
-    MAKE SET  - O(1)              O(1)
-    UNION SET - O(n)           O(alpha(n))
-*/
-//==================================================================
-
-
 struct solution{
-    ll n;
+    int n;
     solution(){
         cin>>n;
         
@@ -118,4 +51,3 @@ int main()
     }
     return 0;
 }
-// myprettyprint.hpp
